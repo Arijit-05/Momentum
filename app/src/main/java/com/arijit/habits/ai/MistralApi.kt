9d31ba023672c1b07ba831c48d4f93422605c400
Arijit-05 <lolboy2005instant@gmail.com>
2025-07-09 13:56:15 +0530

    ai options are enabled again, api key is safely stored in firestore and fetched when making an api call

--- app/src/main/java/com/arijit/habits/ai/MistralApi.kt
+++ app/src/main/java/com/arijit/habits/ai/MistralApi.kt
@@ -4,12 +4,12 @@ import retrofit2.Call
 import retrofit2.http.Body
 import retrofit2.http.Headers
 import retrofit2.http.POST
+import retrofit2.http.Header
 
 interface MistralApi {
-    @Headers(
-        "Authorization: Bearer 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",
-        "Content-Type: application/json"
-    )
     @POST("v1/chat/completions")
-    fun getMistralResponse(@Body request: MistralRequest): Call<MistralResponse>
+    fun getMistralResponse(
+        @Header("Authorization") authHeader: String,
+        @Body request: MistralRequest
+    ): Call<MistralResponse>
 }
